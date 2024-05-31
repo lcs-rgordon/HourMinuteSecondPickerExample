8e319c252cc7e3c6be490a505dd92a083584856a
Russell Gordon <rgordon@lcs.on.ca>
2024-05-31 11:52:18 -0400

    Initial Commit

---
+++ HourMinuteSecondPickerExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HourMinuteSecondPickerExample
+//
+//  Created by Russell Gordon on 2024-05-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HourMinuteSecondPickerExample/HourMinuteSecondPickerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HourMinuteSecondPickerExampleApp.swift
+//  HourMinuteSecondPickerExample
+//
+//  Created by Russell Gordon on 2024-05-31.
+//
+
+import SwiftUI
+
+@main
+struct HourMinuteSecondPickerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
