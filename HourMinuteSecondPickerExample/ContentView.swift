9eb717948968fbd749d9593c550fec52523aafaa
Russell Gordon <rgordon@lcs.on.ca>
2024-05-31 11:58:06 -0400

    Added a minutes picker.

--- HourMinuteSecondPickerExample/ContentView.swift
+++ HourMinuteSecondPickerExample/ContentView.swift
@@ -8,18 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     @State private var hours = 0
+    @State private var minutes = 0
+
+    
     var body: some View {
         VStack(spacing: 20) {
-            Picker("Hours", selection: $hours) {
-                Text("Zero").tag(0)
-                Text("One").tag(1)
-                Text("Two").tag(2)
-                Text("Three").tag(3)
+            HStack {
+                Picker("Hours", selection: $hours) {
+                    Text("Zero").tag(0)
+                    Text("One").tag(1)
+                    Text("Two").tag(2)
+                    Text("Three").tag(3)
+                }
+                .pickerStyle(.wheel)
+
+                Picker("Minutes", selection: $minutes) {
+                    ForEach(0..<61) { value in
+                        Text("\(value)").tag(value)
+                    }
+                }
+                .pickerStyle(.wheel)
+                
             }
-            .pickerStyle(.wheel)
             
             Text("You chose \(hours) hours.")
+            Text("You chose \(minutes) minutes.")
         }
         .font(.title)
     }
